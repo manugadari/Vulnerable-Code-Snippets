c451ab3ce9b69473a15a0cbec113e992d3242cbe
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-06-06 11:28:59 +0530

    Update example.php

--- XSS/example.php
+++ XSS/example.php
@@ -1,7 +1,1 @@
-<?php
 
-require_once('../_helpers/strip.php');
-
-// the `name` variable isn't getting sanitized, making it vulnerable
-// to a reflected Cross-Site Scripting vulnerability.
-echo 'Hello, ' . $_GET['name']
