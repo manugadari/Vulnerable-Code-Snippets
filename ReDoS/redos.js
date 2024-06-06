7764436569d49e3d87450baa22cff0ce3914f16f
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-06-06 18:46:25 +0530

    Update redos.js

--- ReDoS/redos.js
+++ ReDoS/redos.js
@@ -2,13 +2,4 @@ const express = require('express');
 const router = express.Router()
 
 
-router.get("/tstMe", (req, res) => { 
-    var r = /([a-z]+)+$/;
-
-    let match = r.test(req.params.id);
-    res.send(match)
-    
-});
-
-
 module.exports = router
